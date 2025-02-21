01e562cf17557f699ccf96985adc0960adc9c2a4
victor Alomia <alomia18@gmail.com>
2025-02-20 19:02:54 -0500

    updating project

--- src/config/settingDB.ts
+++ src/config/settingDB.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
  dotenv.config({path:'.env.local'})
 const db = new Sequelize(
   `${process.env.URL_CONETION_DB}`,{
-    models:[__dirname + '/../models/**/*.ts'],
+    models:[__dirname + '/../models/**/*'],
     logging:false, //desavilita los logs de sequelize para las pruebas
     dialectOptions:{
         ssl:{
